9a210991de890045ffdb1f00fb737278766d737d
unknown <oyeoluwasegun31@gmail.com>
2024-11-09 09:52:16 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+document.getElementById('toggleSection').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent');
+    let icon = document.getElementById('toggleIcon');
+    let header = document.getElementById('toggleHeader');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+document.getElementById('toggleSection2').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent2');
+    let icon = document.getElementById('toggleIcon2');
+    let header = document.getElementById('toggleHeader2');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+document.getElementById('toggleSection3').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent3');
+    let icon = document.getElementById('toggleIcon3');
+    let header = document.getElementById('toggleHeader3');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+document.getElementById('toggleSection4').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent4');
+    let icon = document.getElementById('toggleIcon4');
+    let header = document.getElementById('toggleHeader4');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+document.getElementById('toggleSection5').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent5');
+    let icon = document.getElementById('toggleIcon5');
+    let header = document.getElementById('toggleHeader5');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+document.getElementById('toggleSection6').addEventListener('click', function() {
+    let content = document.getElementById('toggleContent6');
+    let icon = document.getElementById('toggleIcon6');
+
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        icon.textContent = '-';
+    }
+});
+
+
+
